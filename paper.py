dec9285c85fdaf206c7f2fe8ebc653fabd36f53e
Anna Ho <annaho@mpia.de>
2015-08-11 16:06:02 -0400

    get lsquareds, PAs, PAerrs

--- paper.py
+++ paper.py
@@ -10,3 +10,5 @@ make_starting_file_list.run()
 starting_files = np.loadtxt("starting_files.txt", dtype=str, delimiter=',')
 for filename in starting_files.flatten():
     munge_data.run(filename, 180) # supply the starting guess for RM
+
+# get new RM 
