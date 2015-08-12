4d0fd8d107036b20ee5c2b8317747d1bc2cdd51f
Anna Ho <annaho@mpia.de>
2015-08-12 12:21:09 -0400

    starting guess for 5D is different

--- paper.py
+++ paper.py
@@ -12,9 +12,9 @@ starting_files = np.loadtxt("starting_files.txt", dtype=str, delimiter=',')
 for filename in starting_files.flatten():
     psr = (filename.split('/')[-1]).split('_')[0]
     if psr == 'Ter5D':
-        munge_data.run(filename, 220)
+        munge_data.run(filename, 220) # Ter5D has a much higher RM
     else:
-        munge_data.run(filename, 180) # supply the starting guess for RM
+        munge_data.run(filename, 180) # supply the avg cluster val as RM guess 
 
 # get new RM 
 calc_params.run()
